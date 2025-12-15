44df243bdb5e6619b6b90974938d4bd70f9c3c09
gitbuddhila <buddhilalakshan12@gmail.com>
2025-12-15 11:47:31 +0530

    cc

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,6 @@ export default function Footer() {
     <footer>
       <div className="footer-content">
         <p>© {currentYear} Buddhila Herath. All rights reserved.</p>
-        <p className="footer-email">
-          <a href="mailto:buddhilalakshan12@gmail.com">buddhilalakshan12@gmail.com</a>
-        </p>
       </div>
     </footer>
   );
